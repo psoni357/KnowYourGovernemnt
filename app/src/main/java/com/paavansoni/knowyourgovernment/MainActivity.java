e42b73d6a46bccc3a232b8c1e7afbc71be8cd4a6
paavansoni <psoni1@hawk.iit.edu>
2020-03-10 20:19:47 -0500

    first commit

---
+++ app/src/main/java/com/paavansoni/knowyourgovernment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.paavansoni.knowyourgovernment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
