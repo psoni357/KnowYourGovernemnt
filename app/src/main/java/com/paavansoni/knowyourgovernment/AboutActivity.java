8c81007137a658f1971e7871ad18f5da529c06fb
paavansoni <psoni1@hawk.iit.edu>
2020-03-24 15:47:34 -0500

    about page now links to API page

--- app/src/main/java/com/paavansoni/knowyourgovernment/AboutActivity.java
+++ app/src/main/java/com/paavansoni/knowyourgovernment/AboutActivity.java
@@ -2,13 +2,27 @@ package com.paavansoni.knowyourgovernment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private static final String API_LINK = "https://developers.google.com/civic-information/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView api = findViewById(R.id.APILink);
+
+    }
+
+    public void launchLink(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(API_LINK));
+        startActivity(intent);
     }
 }
