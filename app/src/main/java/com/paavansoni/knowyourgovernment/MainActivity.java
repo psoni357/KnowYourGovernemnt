6b12492591255e3ccc9b07105a436763624dbb37
paavansoni <psoni1@hawk.iit.edu>
2020-03-29 18:34:33 -0500

    implemented UpNav

--- app/src/main/java/com/paavansoni/knowyourgovernment/MainActivity.java
+++ app/src/main/java/com/paavansoni/knowyourgovernment/MainActivity.java
@@ -22,6 +22,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.JsonWriter;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
@@ -31,7 +32,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -53,6 +64,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private LocationManager locationManager;
     private Criteria criteria;
 
+    private static boolean comingBack = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,7 +76,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         if(doNetCheck()){
-            startLoc();
+            if(comingBack){
+                try{
+                    InputStream is = getApplicationContext().
+                            openFileInput("list.json");
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    JSONArray notes = new JSONArray(sb.toString());
+                    politicianList.clear();
+                    for (int i = 0; i < notes.length(); i++){
+                        JSONObject pol = notes.getJSONObject(i);
+                        if(i==0){
+                            TextView loc = findViewById(R.id.location);
+                            loc.setText(pol.getString("Location"));
+                        }
+                        else{
+                            String office = pol.getString("Office");
+                            String name = pol.getString("Name");
+                            String party = pol.getString("Party");
+
+                            String address = pol.getString("Address");
+                            String number = pol.getString("Number");
+                            String email = pol.getString("Email");
+                            String website = pol.getString("Website");
+
+                            String facebook = pol.getString("Facebook");
+                            String twitter = pol.getString("Twitter");
+                            String google = pol.getString("Google");
+                            String youtube = pol.getString("Youtube");
+
+                            String photo = pol.getString("Photo");
+                            Politician p = new Politician(office, name, party, address, number, email, website, facebook, twitter, google, youtube, photo);
+                            politicianList.add(p);
+                        }
+                    }
+                    mAdapter.notifyDataSetChanged();
+                    comingBack = false;
+
+                }
+                catch (FileNotFoundException e){
+                    startLoc();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    startLoc();
+                }
+            }
+            else{
+                startLoc();
+            }
+
+
         }
         else{
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -166,6 +236,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (item.getItemId()){
             case R.id.about:
                 //launch activity
+                leaving();
                 Intent intent = new Intent(this, AboutActivity.class);
                 startActivity(intent);
                 return true;
@@ -220,6 +291,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        leaving();
         pos = recyclerView.getChildLayoutPosition(v);
         p = politicianList.get(pos);
 
@@ -260,5 +332,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
 
         return netInfo != null && netInfo.isConnected();
+    }
+
+    public void leaving() {
+        comingBack = true;
+        TextView loc = findViewById(R.id.location);
+        try {
+            FileOutputStream fos = getApplicationContext().
+                    openFileOutput("list.json", Context.MODE_PRIVATE);
+            JsonWriter writer = new JsonWriter(new OutputStreamWriter(fos, StandardCharsets.UTF_8));
+            writer.setIndent("  ");
+            writer.beginArray();
+
+            writer.beginObject();
+            writer.name("Location").value(loc.getText().toString());
+            writer.endObject();
+
+            for(Politician n: politicianList){
+                writer.beginObject();
+                writer.name("Office").value(n.getOffice());
+                writer.name("Name").value(n.getName());
+                writer.name("Party").value(n.getParty());
+                writer.name("Address").value(n.getAddress());
+                writer.name("Number").value(n.getNumber());
+                writer.name("Email").value(n.getEmail());
+                writer.name("Website").value(n.getWebsite());
+                writer.name("Facebook").value(n.getFacebook());
+                writer.name("Twitter").value(n.getTwitter());
+                writer.name("Google").value(n.getGoogle());
+                writer.name("Youtube").value(n.getYoutube());
+                writer.name("Photo").value(n.getPhoto());
+                writer.endObject();
+            }
+            writer.endArray();
+            writer.close();
+        }
+        catch(Exception e){
+            e.getStackTrace();
+        }
+
+        super.onPause();
     }
 }
